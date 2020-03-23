93cf46adc160a8229f9c1c816e8b582a47c63ed9
JohnHe <hechunwang2000327@hotmail.com>
2020-03-23 12:19:54 +0800

    Lab1: Try useful commands

---
+++ Information Security/lab1.bat
@@ -0,0 +1,23 @@
+cd %userprofile%/Documents
+
+tracert cybersecurity.ustc.edu.cn
+
+dir
+
+md cybersecurity_lab1
+dir
+
+copy "restart.bat" "./cybersecurity_lab1"
+dir cybersecurity_lab1
+
+net
+net user
+net config
+net view
+net share
+net share C$
+net share /help
+
+ipconfig
+
+netstat
