c6ff684339794efc32b277580464a32c8ed1d8fd
JohnHe <hechunwang2000327@hotmail.com>
2020-03-27 18:43:04 +0800

    finish BranchDecision.v

--- Computer Architecture/SourceCode/BranchDecision.v
+++ Computer Architecture/SourceCode/BranchDecision.v
@@ -2,7 +2,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Company: USTC ESLAB
 // Engineer: Huang Yifan (hyf15@mail.ustc.edu.cn)
-// 
+//           John He (hechunwang2000327@hotmail.com)
 // Design Name: RV32I Core
 // Module Name: Branch Decision
 // Tool Versions: Vivado 2017.4.1
@@ -29,6 +29,22 @@ module BranchDecision(
     output reg br
     );
 
-    // TODO: Complete this module
-
+    // DONE: Complete this module
+    always@(*)
+    begin
+        case (br_type)
+            `NOBRANCH: br <= 0;
+            `BEQ: br <= (reg1 == reg2) ? 1 : 0;
+            `BNE: br <= (reg1 != reg2) ? 1 : 0;
+            `BLT: br <= (reg1[31] & ~reg2[31]) ? 1 :
+                            (~reg1[31] & reg2[31]) ? 0 :
+                                (reg1 < reg2) ? 1 : 0;
+            `BLTU: br <= (reg1 < reg2) ? 1 : 0;
+            `BGE: br <= (reg1[31] & ~reg2[31]) ? 0 :
+                            (~reg1[31] & reg2[31]) ? 1 :
+                                (reg1 >= reg2) ? 1 : 0;
+            `BGEU: br <= (reg1 >= reg2) ? 1 : 0;
+            default: br <= 0;
+        endcase
+    end
 endmodule
