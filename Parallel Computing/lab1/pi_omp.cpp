ea800bdc3040a161b61f0d5695a1f61b9d9cbb2a
JohnHe <hechunwang2000327@hotmail.com>
2020-06-30 22:15:29 +0800

    change encoding

--- Parallel Computing/lab1/pi_omp.cpp
+++ Parallel Computing/lab1/pi_omp.cpp
@@ -1,4 +1,4 @@
-﻿#include "stdlib.h"
+#include <stdlib.h>
 #include <stdio.h>
 #include <tchar.h>
 #include <omp.h>
@@ -9,22 +9,22 @@ double step;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	int i;
-	double x, pi, sum = 0.0;
-	step = 1.0 / (double)num_steps;
-	time_t tStart, tEnd;
-	tStart = clock();
-	omp_set_num_threads(NUM_THREADS);
-	#pragma omp parallel for reduction(+:sum) private(x)
-	for (i = 0; i < num_steps; i++)
-	{
-		x = (i + 0.5) * step;
-		sum += 4.0 / (1.0 + x * x);
-	}
-	pi = step * sum;
-	tEnd = clock();
+    int i;
+    double x, pi, sum = 0.0;
+    step = 1.0 / (double)num_steps;
+    time_t t_start, t_end;
+    t_start = clock();
+    omp_set_num_threads(NUM_THREADS);
+    #pragma omp parallel for reduction(+:sum) private(x)
+    for (i = 0; i < num_steps; i++)
+    {
+        x = (i + 0.5) * step;
+        sum += 4.0 / (1.0 + x * x);
+    }
+    pi = step * sum;
+    t_end = clock();
 
-	printf("pi = %f\n", pi);
-	printf("time used: %lf s\n", (double)(tEnd - tStart) / CLOCKS_PER_SEC);
-	return 0;
+    printf("pi = %lf\n", pi);
+    printf("time used: %lf s\n", (double)(t_end - t_start) / CLOCKS_PER_SEC);
+    return 0;
 }
